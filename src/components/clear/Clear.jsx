aaf9a64e901d34843314aea4f0b190f22964e616
Yaeyae711 <99589875+Yaeyae711@users.noreply.github.com>
2022-04-07 12:33:57 +0530

    Delete src directory

--- src/components/clear/Clear.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles1 from "./clear.module.css"
-export default function Clear({clr}) {
-  return (
-    <div><button className= {styles1.clrColor} onClick={clr} >Clear</button>
-
-     </div>
-  )
-}
--- src/components/subtraction/Subtraction.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles2 from "./sub.module.css"
-
-export default function Subtraction({sub}) {
-  return (
-    <div><button className={styles2.subColor}  onClick={sub}>Subtract</button></div>
-  )
-}
